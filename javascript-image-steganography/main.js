df4ac52d08bbeda996b08cdfe38fab720a9c673b
Brady Anderson <bradya48@gmail.com>
2018-11-12 00:14:31 -0600

    complete encode/decode pixel functions and encode/decode length

--- javascript-image-steganography/main.js
+++ javascript-image-steganography/main.js
@@ -23,26 +23,66 @@ function setCanvasDims(canvas, img) {
   canvas.height = Math.round(new_dims.h);
 }
 
-function getPixelData(ctx, x, y) {
-  return ctx.getImageData(x, y, 1, 1).data;
+function getPixelData(ctx, location) {
+  return ctx.getImageData(location.x, location.y, 1, 1).data;
 }
 
-function changePixelData(ctx, x, y) {
+function changePixelData(ctx, location, pixelData) {
+  ctx.fillStyle = "rgba(" + pixelData[0] + "," + pixelData[1] + "," + pixelData[2] + "," + pixelData[3] + ")";
+  ctx.fillRect(location.x, location.y, 1, 1 );
+}
 
+function encodePixel(ctx, location, data) {
+  var newPixelData = getPixelData(ctx, location);
+
+  for (var i = 0; i < 3; i++) {
+    var curBinary = zeroFill(toBinary(newPixelData[i]), 8);
+    var newBinary = curBinary;
+    newBinary = newBinary.slice(0,6).concat(data[i*2] || 0, data[i*2 + 1] || 0);
+    newPixelData[i] = toDecimal(newBinary);
+  }
+  changePixelData(ctx, location, newPixelData)
+}
+
+function decodePixel(ctx, location) {
+  var pixelData = getPixelData(ctx, location);
+
+  var decode = "";
+  for (var i = 0; i < 3; i++) {
+    var binary = zeroFill(toBinary(pixelData[i]), 8);
+    decode = decode + binary.slice(6);
+  }
+  return decode;
 }
 
 function updateAfter() {
   var message = document.getElementById('message-input').value;
-  updateLength(message.length);
+  encodeLength(message.length);
+  console.log(decodeLength());
 }
 
-function updateLength(newLength) {
+function encodeLength(newLength) {
   var binaryLengthStr = zeroFill(toBinary(newLength), 32);
-  console.log(binaryLengthStr);
+  var tempStr = binaryLengthStr;
+
+  var i = 0;
+  while (tempStr.length > 0) {
+    var slice = tempStr.slice(0, 6);
+    var tempStr = tempStr.slice(6);
+    encodePixel(actx, getCanvasLocation(ac, i), slice);
+    i++;
+  }
 }
 
-function readLength() {
+function decodeLength() {
 
+  var lengthBin = ""
+  var i = 0;
+  while (lengthBin.length < 32) {
+    lengthBin = lengthBin + decodePixel(actx, getCanvasLocation(ac, i));
+    i++;
+  }
+  return toDecimal(lengthBin.slice(0,32));
 }
 
 function toBinary(decNum) {
@@ -64,10 +104,11 @@ function toDecimal(binary) {
 }
 
 function getCanvasLocation(c, n) {
-  var location = {x: c.width % c.width, y: Math.floor(n / c.width)};
+  var location = {x: n % c.width, y: Math.floor(n / c.width)};
   if (location.y > c.height) {
     return -1;
   }
+  
   return location;
 }
 
